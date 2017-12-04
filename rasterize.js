e2ecdc5f70febc2edd792429c2d47d4d3f83f589
Ryan Mee <rpmee@ncsu.edu>
2017-12-04 14:54:24 -0500

    imgs

--- rasterize.js
+++ rasterize.js
@@ -56,8 +56,8 @@ var viewDelta = 0; // how much to displace view with each key press
 
 // textures
 var textures = new Array()  // array for holding textures, [tag: '', src: '', texture: WebGLTexture]
-var pngs = ['abe', 'earth', 'leaf.small', 'tree', 'reticle']
-var jpgs = ['billie', 'retro', 'rocktile', 'sky', 'stars', 'sun']
+var pngs = ['abe', 'leaf.small', 'tree', 'shot', 'reticle']
+var jpgs = ['billie', 'retro', 'rocktile', 'sky', 'stars', 'earth', 'sun']
 var gifs = ['glass']
 
 // game variables
